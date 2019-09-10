9c4f63b9fabffeeb7d000b66db6ebdf40cdb5207
Revolver Ocelot(Revolver Ocelot) <47671687+RevolverOcelotIII@users.noreply.github.com>
2019-09-10 15:52:02 -0300

    Projeto base

---
+++ ViewsTeste/Models/MaterialModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewsTeste.Models
+{
+    public class MaterialModel
+    {
+        public int ID { get; set; }
+        [Display(Name = "Pião")]
+        public string nome { get; set; }
+        public double valor { get; set; }
+        public double quantidade { get; set; }
+        public string tipo { get; set; }
+    }
+}
